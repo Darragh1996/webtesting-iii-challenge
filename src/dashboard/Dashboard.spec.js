081780e951a63f9ac3ba72b8c23978c7c8faff3c
Darragh Ferry <darraghferry@hotmail.com>
2019-12-16 15:55:49 +0000

    misc changes

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -40,49 +40,47 @@ it("renders without crashing", () => {
   expect(wrapper.container).toMatchSnapshot();
 });
 
-it('renders expected elements properly-> DEFAULT', () => {
-    expect(wrapper.container).toMatchSnapshot();
-    expect(Unlocked()).toBeInTheDocument();
-    expect(Unlocked()).toBeVisible();
-    expect(Open()).toBeInTheDocument();
-    expect(Open()).toBeVisible();
-    expect(LockGate()).toBeInTheDocument();
-    expect(LockGate()).toBeVisible();
-    expect(LockGate()).toBeDisabled();
-    expect(CloseGate()).toBeInTheDocument();
-    expect(CloseGate()).toBeVisible();
+it("renders expected elements properly-> DEFAULT", () => {
+  expect(wrapper.container).toMatchSnapshot();
+  expect(Unlocked()).toBeInTheDocument();
+  expect(Unlocked()).toBeVisible();
+  expect(Open()).toBeInTheDocument();
+  expect(Open()).toBeVisible();
+  expect(LockGate()).toBeInTheDocument();
+  expect(LockGate()).toBeVisible();
+  expect(LockGate()).toBeDisabled();
+  expect(CloseGate()).toBeInTheDocument();
+  expect(CloseGate()).toBeVisible();
 });
 
-it('renders expected elements properly -> AFTER CLOSE GATE BUTTON IS CLICKED',
- () => {
-    rtl.fireEvent.click(CloseGate());
-    expect(wrapper.container).toMatchSnapshot();
-    expect(Unlocked()).toBeInTheDocument();
-    expect(Unlocked()).toBeVisible();
-    expect(Closed()).toBeInTheDocument();
-    expect(Closed()).toBeVisible();
-    expect(Open()).not.toBeInTheDocument();
-    expect(LockGate()).toBeInTheDocument();
-    expect(LockGate()).toBeVisible();
-    expect(LockGate()).not.toBeDisabled();
-    expect(OpenGate()).toBeInTheDocument();
-    expect(OpenGate()).toBeVisible();
+it("renders expected elements properly -> AFTER CLOSE GATE BUTTON IS CLICKED", () => {
+  rtl.fireEvent.click(CloseGate());
+  expect(wrapper.container).toMatchSnapshot();
+  expect(Unlocked()).toBeInTheDocument();
+  expect(Unlocked()).toBeVisible();
+  expect(Closed()).toBeInTheDocument();
+  expect(Closed()).toBeVisible();
+  expect(Open()).not.toBeInTheDocument();
+  expect(LockGate()).toBeInTheDocument();
+  expect(LockGate()).toBeVisible();
+  expect(LockGate()).not.toBeDisabled();
+  expect(OpenGate()).toBeInTheDocument();
+  expect(OpenGate()).toBeVisible();
 });
 
-it('renders expected elements properly -> AFTER CLOSE GATE WAS CLICKED AND LOCK GATE WAS THEN CLICKED',
- () => {
-    rtl.fireEvent.click(CloseGate());
-    rtl.fireEvent.click(LockGate());
-    expect(wrapper.container).toMatchSnapshot();
-    expect(Unlocked()).not.toBeInTheDocument();
-    expect(Locked()).toBeInTheDocument();
-    expect(Locked()).toBeVisible();
-    expect(Closed()).toBeInTheDocument();
-    expect(Closed()).toBeVisible();
-    expect(LockGate()).not.toBeInTheDocument();
-    expect(UnlockGate()).toBeInTheDocument();
-    expect(UnlockGate()).toBeVisible();
-    expect(OpenGate()).toBeInTheDocument();
-    expect(OpenGate()).toBeVisible();
-    expect(OpenGate()).toBeDisabled();
+it("renders expected elements properly -> AFTER CLOSE GATE WAS CLICKED AND LOCK GATE WAS THEN CLICKED", () => {
+  rtl.fireEvent.click(CloseGate());
+  rtl.fireEvent.click(LockGate());
+  expect(wrapper.container).toMatchSnapshot();
+  expect(Unlocked()).not.toBeInTheDocument();
+  expect(Locked()).toBeInTheDocument();
+  expect(Locked()).toBeVisible();
+  expect(Closed()).toBeInTheDocument();
+  expect(Closed()).toBeVisible();
+  expect(LockGate()).not.toBeInTheDocument();
+  expect(UnlockGate()).toBeInTheDocument();
+  expect(UnlockGate()).toBeVisible();
+  expect(OpenGate()).toBeInTheDocument();
+  expect(OpenGate()).toBeVisible();
+  expect(OpenGate()).toBeDisabled();
 });
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -2,7 +2,7 @@
 import React from "react";
 import * as rtl from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import Display from './Display';
+import Display from "./Display";
 
 afterEach(rtl.cleanup);
 
@@ -12,57 +12,56 @@ let wrapper;
 // });
 
 let Unlocked = () => {
-    return wrapper.queryByText("Unlocked");
-  };
+  return wrapper.queryByText("Unlocked");
+};
 
-  let Open = () => {
-    return wrapper.queryByText("Open");
-  };
-  
-  let Closed = () => {
-    return wrapper.queryByText("Closed");
-  };
-  let Locked = () => {
-    return wrapper.queryByText("Locked");
-  };
+let Open = () => {
+  return wrapper.queryByText("Open");
+};
 
-it('renders without crashing',() => {
-    wrapper = rtl.render(<Display />);
-    expect(wrapper.container).toMatchSnapshot();
+let Closed = () => {
+  return wrapper.queryByText("Closed");
+};
+let Locked = () => {
+  return wrapper.queryByText("Locked");
+};
+
+it("renders without crashing", () => {
+  wrapper = rtl.render(<Display />);
+  expect(wrapper.container).toMatchSnapshot();
 });
 
-it('renders elements properly -> DEFUALT PROPS', () => {
-    wrapper = rtl.render(<Display closed={false} locked={false} />);
-    expect(wrapper.container).toMatchSnapshot();
-    expect(Unlocked()).toBeInTheDocument();
-    expect(Unlocked()).toBeVisible();
-    expect(Unlocked()).toHaveClass('led', 'green-led')
-    expect(Open()).toBeInTheDocument();
-    expect(Open()).toBeVisible();
-    expect(Open()).toHaveClass('led', 'green-led');
+it("renders elements properly -> DEFUALT PROPS", () => {
+  wrapper = rtl.render(<Display closed={false} locked={false} />);
+  expect(wrapper.container).toMatchSnapshot();
+  expect(Unlocked()).toBeInTheDocument();
+  expect(Unlocked()).toBeVisible();
+  expect(Unlocked()).toHaveClass("led", "green-led");
+  expect(Open()).toBeInTheDocument();
+  expect(Open()).toBeVisible();
+  expect(Open()).toHaveClass("led", "green-led");
 });
 
-it('renders elements properly -> CLOSED SET TO TRUE', () => {
-    wrapper = rtl.render(<Display closed={true} locked={false} />);
-    expect(wrapper.container).toMatchSnapshot();
-    expect(Unlocked()).toBeInTheDocument();
-    expect(Unlocked()).toBeVisible();
-    expect(Unlocked()).toHaveClass('led', 'green-led')
-    expect(Open()).not.toBeInTheDocument();
-    expect(Closed()).toBeInTheDocument();
-    expect(Closed()).toBeVisible();
-    expect(Closed()).toHaveClass('led', 'red-led');
+it("renders elements properly -> CLOSED SET TO TRUE", () => {
+  wrapper = rtl.render(<Display closed={true} locked={false} />);
+  expect(wrapper.container).toMatchSnapshot();
+  expect(Unlocked()).toBeInTheDocument();
+  expect(Unlocked()).toBeVisible();
+  expect(Unlocked()).toHaveClass("led", "green-led");
+  expect(Open()).not.toBeInTheDocument();
+  expect(Closed()).toBeInTheDocument();
+  expect(Closed()).toBeVisible();
+  expect(Closed()).toHaveClass("led", "red-led");
 });
 
-it('renders elements properly -> CLOSED & LOCKED SET TO TRUE', () => {
-    wrapper = rtl.render(<Display closed={true} locked={true} />);
-    expect(Unlocked()).not.toBeInTheDocument();
-    expect(Locked()).toBeInTheDocument();
-    expect(Locked()).toBeVisible();
-    expect(Locked()).toHaveClass('led', 'red-led')
-    expect(Open()).not.toBeInTheDocument();
-    expect(Closed()).toBeInTheDocument();
-    expect(Closed()).toBeVisible();
-    expect(Closed()).toHaveClass('led', 'red-led');
+it("renders elements properly -> CLOSED & LOCKED SET TO TRUE", () => {
+  wrapper = rtl.render(<Display closed={true} locked={true} />);
+  expect(Unlocked()).not.toBeInTheDocument();
+  expect(Locked()).toBeInTheDocument();
+  expect(Locked()).toBeVisible();
+  expect(Locked()).toHaveClass("led", "red-led");
+  expect(Open()).not.toBeInTheDocument();
+  expect(Closed()).toBeInTheDocument();
+  expect(Closed()).toBeVisible();
+  expect(Closed()).toHaveClass("led", "red-led");
 });
-
